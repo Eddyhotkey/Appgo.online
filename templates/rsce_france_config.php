0d8e1b8c56b6de6832e1d68ddd3dc5d51a614eb2
Eddyhotkey <rshdbairamov@gmail.com>
2023-06-23 11:12:08 +0200

    Backend Template konfigurieren: Felder (Bild, Text, Checkbox, Link ) wurden hinzufügt.

--- templates/rsce_france_config.php
+++ templates/rsce_france_config.php
@@ -7,7 +7,7 @@
 
 return array(
   'label' => array(
-    'Merkmale-Liste: icon, text',
+    'Merkmale-Liste: Icon, Teaser-Text',
   ),
   'types' => array( 'content' ),
   'contentCategory' => 'RSCE: Main',
